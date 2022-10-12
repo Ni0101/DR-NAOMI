210464da5934b1d8c1c8a97db7c81952c5a997f7
Ni0101 <96425182+Ni0101@users.noreply.github.com>
2022-10-12 16:49:07 +0700

    slick, section2

---
+++ slick.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.section-1_lists').slick({
+        infinite: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 6,
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: true,
+        centerMode: true,
+    });
+
+    $('.section-2_lists').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 5.5,
+        prevArrow: `<button type='button' class='slick-prev pull-left'><ion-icon name="chevron-back-circle-outline"></ion-icon></button>`,
+        nextArrow: `<button type='button' class='slick-next pull-right'><ion-icon name="chevron-forward-circle-outline"></ion-icon></button>`,
+    });
+});
