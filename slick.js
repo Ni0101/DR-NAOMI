51915c29e0c7fb437cb4eabaf0427af0337eea64
Ni0101 <96425182+Ni0101@users.noreply.github.com>
2022-10-14 01:48:50 +0700

    section 3,4,5,6,7,8,9

--- slick.js
+++ slick.js
@@ -13,7 +13,16 @@ $(document).ready(function(){
         infinite: true,
         autoplay: true,
         autoplaySpeed: 1000,
-        slidesToShow: 5.5,
+        slidesToShow: 6,
+        prevArrow: `<button type='button' class='slick-prev pull-left'><ion-icon name="chevron-back-circle-outline"></ion-icon></button>`,
+        nextArrow: `<button type='button' class='slick-next pull-right'><ion-icon name="chevron-forward-circle-outline"></ion-icon></button>`,
+    });
+
+    $('.section-4_lists').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 6,
         prevArrow: `<button type='button' class='slick-prev pull-left'><ion-icon name="chevron-back-circle-outline"></ion-icon></button>`,
         nextArrow: `<button type='button' class='slick-next pull-right'><ion-icon name="chevron-forward-circle-outline"></ion-icon></button>`,
     });
